7fb10017f4cde6516eaa2a1a027cb64492882e69
JKaraky <jawad.alkaraky@gmail.com>
2023-07-25 23:17:18 +0300

    rotation script edit

--- Assets/Scripts/Alcoholism/Rotator.cs
+++ Assets/Scripts/Alcoholism/Rotator.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     [SerializeField] int rotationSpeed;
-    [SerializeField][Range(-1, 1)] int rotateLeft;
+    [SerializeField][Range(-1, 1)] int rotation;
+    [SerializeField] bool altRotation = false;
+    [SerializeField] float rotInterval;
+
+    float timeElapsed = 0;
+    int rotSwitcher = 1;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * rotateLeft * rotationSpeed * Time.deltaTime);
+        if (altRotation)
+        {
+            if (timeElapsed < rotInterval)
+            {
+                transform.Rotate(Vector3.forward * rotation * rotSwitcher * rotationSpeed * Time.deltaTime);
+                timeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                timeElapsed = 0;
+                rotSwitcher *= -1;
+            }
+        }
+        else
+        {
+            transform.Rotate(Vector3.forward * rotation * rotationSpeed * Time.deltaTime);
+        }
     }
 }
