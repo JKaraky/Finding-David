8e2a9644ee8e1537ab65e0b31061dda04af98729
JKaraky <jawad.alkaraky@gmail.com>
2024-07-22 15:01:47 +0300

    Fear entity fixed

--- Assets/Scripts/Fear/Enemies/Fear Entity/FearEntity.cs
+++ Assets/Scripts/Fear/Enemies/Fear Entity/FearEntity.cs
@@ -10,19 +10,15 @@ public class FearEntity : MonoBehaviour
 
     [SerializeField] FearGameManager gameManager;
     [SerializeField] GameObject player;
-    [SerializeField] GameObject entityToMove;
     [SerializeField] GameObject entityGoal;
 
-    [SerializeField] float distanceFromPlayer;
+    [SerializeField] float distanceFromPlayerX;
+    [SerializeField] float distanceFromPlayerY;
     [SerializeField] float fixedSpeed;
 
     float variableSpeed;
-    int playerFollowOrPlat;
-    bool entityCanMove;
 
     GameObject playerFollow;
-    GameObject platformToBreak;
-    PlayerClone playerCloneScript;
     Vector3 currentRotation;
 
     public float VariableSpeed
@@ -43,30 +39,23 @@ public class FearEntity : MonoBehaviour
     private void Awake()
     {
         playerFollow = entityGoal; // Saving the player destination
-        playerCloneScript = gameObject.GetComponentInParent<PlayerClone>();
     }
     void OnEnable()
     {
-        entityCanMove = true;
-        // Determine whether entity will attack player or make a platform breakable (0 for player, 1 for platform)
-        playerFollowOrPlat = UnityEngine.Random.Range(0, 2);
-
-        DetermineEntityTask(playerFollowOrPlat);
-
         variableSpeed = fixedSpeed;
         // Getting rotation of entity
         currentRotation = transform.rotation.eulerAngles;
 
         if (player.transform.localScale.x > 0)
         {
-            entityToMove.transform.position = entityGoal.transform.position - new Vector3(distanceFromPlayer, 0, 0);
+            transform.position = entityGoal.transform.position - new Vector3(distanceFromPlayerX, distanceFromPlayerY, 0);
             // Flipping entity
             currentRotation.y = 0f;
             transform.rotation = Quaternion.Euler(currentRotation);
         }
         else
         {
-            entityToMove.transform.position = entityGoal.transform.position + new Vector3(distanceFromPlayer, 0, 0);
+            transform.position = entityGoal.transform.position + new Vector3(distanceFromPlayerX, 0, 0) - new Vector3(0, distanceFromPlayerY, 0);
             // Flipping it back
             currentRotation.y = 180f;
             transform.rotation = Quaternion.Euler(currentRotation);
@@ -75,11 +64,8 @@ public class FearEntity : MonoBehaviour
 
     void Update()
     {
-        if (entityCanMove)
-        {
-            float step = variableSpeed * Time.deltaTime;
-            entityToMove.transform.position = Vector3.MoveTowards(entityToMove.transform.position, entityGoal.transform.position, step);
-        }
+        float step = variableSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, entityGoal.transform.position - new Vector3(0, distanceFromPlayerY, 0), step);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -87,34 +73,8 @@ public class FearEntity : MonoBehaviour
         if (collision.CompareTag("EntityGoal"))
         {
             ReachedGoal?.Invoke();
-            //gameObject.SetActive(false);
-        }
-        // Entity stops moving when it's within a certain distance of the player so it can catch him
-        else if (collision.CompareTag("StopEntityTrigger"))
-        {
-            entityCanMove = false;
-        }
-        else if (playerFollowOrPlat==1 && collision.gameObject.layer == 8) // Hits a platform
-        {
+            Debug.Log("Game Over Man");
             gameObject.SetActive(false);
-            collision.gameObject.AddComponent<PlatBreak>();
-        }
-    }
-
-    private void DetermineEntityTask(int toggle)
-    {
-        if (toggle == 0)
-        {
-            entityGoal = playerFollow;
-
-            //playerCloneScript.enabled = true; // Follow player 
-        }
-        else
-        {
-            platformToBreak = gameManager.GetRandomPlatform();
-            entityGoal = platformToBreak;
-
-            playerCloneScript.enabled = false; // Don't follow player
         }
     }
 }
